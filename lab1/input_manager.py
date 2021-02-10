e133c5624a34995075ba8621c32fb9eace0e69d7
VladyslavLopata <lopatavlad@gmail.com>
2021-02-10 14:18:35 +0200

    added get_task_data

---
+++ lab1/input_manager.py
@@ -0,0 +1,11 @@
+from lab1.model import Alternative, Expert, TaskData
+
+
+def get_task_data() -> TaskData:
+    exp1 = Expert(name = 'Vasia', grades=[1,2,3])
+    exp2 = Expert(name = 'Petia', grades=[3,2,3])
+
+    alt1 = Alternative(name='UWU')
+    alt2 = Alternative(name='OWO')
+
+    return TaskData(alternatives=[alt1, alt2], experts=[exp1, exp2])
