7ea6217d172bde7b775e987005297438ad79a2ce
DESKTOP-M7N9D2E\iliaf <iliafpyx@gmail.com>
2021-02-10 15:25:10 +0200

    CommentsBaby

--- lab1/maintest.py
+++
@@ -1,3 +0,0 @@
-import math_manager as mm
-
-print(mm.Solve())
--- lab1/math_manager.py
+++ lab1/math_manager.py
@@ -1,23 +1,29 @@
-from input_manager import get_task_data as GTD
-import model
+from lab1.input_manager import get_task_data as GTD
+import lab1.model
 import numpy as np
 
 
 def Solve():
+    #Получить всю инфу
     td = GTD()
 
+    #Первая строка матрици
     matrix = np.array(td.experts[0].grades)
 
+    #Все остальные строки
     for grades in td.experts[1:]:
-        np.concatenate(matrix, grades, axis=0)
+        np.concatenate(matrix, grades)
 
+    #Сума оценок альтернатив у 1го експерта
     sumy = []
+    #Нормированые оценки альтерантив
     finals = []
 
+    #Вычисление суммы оценок альтеранатив
     for row in matrix:
         sumy.append(sum(row))
 
-
+    #Вычисление нормированых оценок альтернатив
     for column in matrix.transpose():
         a = 0.
         for elem,elemss in zip(column, sumy):
